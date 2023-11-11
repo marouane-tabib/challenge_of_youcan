d06f813d425da0ab69f2b7574bff7a4a7c47f0ea
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-11 15:34:32 +0100

    MakeAbstractsDerictory |  Add repository property and set its value in the constructor after injecting the object with BaseResourceRepositoryInterface class type. Also, add index method - Abstracts/AbstractBaseResourceService

--- app/Abstracts/AbstractBaseResourceService.php
+++ app/Abstracts/AbstractBaseResourceService.php
@@ -2,6 +2,20 @@
 
 namespace App\Abstracts;
 
+use App\Interfaces\BaseResourceRepositoryInterface;
 use App\Interfaces\BaseResourceServiceInterface;
 
-abstract class AbstractBaseResourceService implements BaseResourceServiceInterface {}
+abstract class AbstractBaseResourceService implements BaseResourceServiceInterface
+{
+    protected BaseResourceRepositoryInterface $repository;
+
+    public function __construct(BaseResourceRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function index(array $data = [])
+    {
+        return $this->repository->filter($data);
+    }
+}
