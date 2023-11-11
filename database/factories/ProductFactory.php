78b80310b7fdda427ccf1926ca9a6370bd3faddb
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-11 02:01:47 +0100

    YC-1 | Add product data with Faker library - factories/ProductFactory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -17,7 +17,10 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->name(),
+            'description' => fake()->text(),
+            'price' => fake()->numberBetween(200 , 700),
+            'category_id' => fake()->numberBetween(1 , 5),
         ];
     }
 }
