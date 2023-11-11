e7da7db670c94364ee11914206c2b0b784f69166
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-11 01:56:27 +0100

    YC-1 | Add columns of the products table - migrations/create_products_table

--- database/migrations/2023_11_11_004052_create_products_table.php
+++ database/migrations/2023_11_11_004052_create_products_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('image')->default('default-img.jpg');
+            $table->string('name', 55);
+            $table->text('description', 5000);
+            $table->decimal('price', 8, 2);
+            $table->unsignedBigInteger('category_id');
+            // $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
