261b8c86fe85dac1bcc7da5d1085a5799ff3d800
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-11 03:35:58 +0100

    MakeAbstractsDerictory | Add modal property and set its value in the constructor after injecting the object with Modal class type. Also, add all methods - app/Abstracts/AbstractBaseResourceRepository

--- app/Abstracts/AbstractBaseResourceRepository.php
+++ app/Abstracts/AbstractBaseResourceRepository.php
@@ -3,5 +3,19 @@
 namespace App\Abstracts;
 
 use App\Interfaces\BaseResourceRepositoryInterface;
+use Illuminate\Database\Eloquent\Model;
 
-abstract class BaseResourceRepository implements BaseResourceRepositoryInterface {}
+abstract class AbstractBaseResourceRepository implements BaseResourceRepositoryInterface
+{
+    protected Model $model;
+
+    public function __construct(Model $model)
+    {
+        $this->model = $model;
+    }
+
+    public function all()
+    {
+        return $this->model->all();
+    }
+}
