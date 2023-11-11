a95244ec062c104d1ade63589c8dd42b68e5f8b0
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-11 02:50:06 +0100

    MakeInterfacesDerictory | Delete filter & create method - Interfaces/BaseResourceRepositoryInterface

--- app/Interfaces/BaseResourceRepositoryInterface.php
+++ app/Interfaces/BaseResourceRepositoryInterface.php
@@ -5,8 +5,4 @@ namespace App\Interfaces;
 interface BaseResourceRepositoryInterface
 {
     public function all();
-
-    public function create(array $data);
-
-    public function filter(array $filter);
 }
