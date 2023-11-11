97225499d7b33867b6fe6922277edc66d6224a1f
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-11 02:06:23 +0100

    YC-1 | Add fillable property to Models/Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'image',
+        'name',
+        'description',
+        'price',
+        'category_id'
+    ];
 }
