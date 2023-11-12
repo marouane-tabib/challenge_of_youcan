441d09b7f1165035a2d3e312705a92e23a3d0247
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-12 04:52:32 +0100

    YC-1 | Delete all methods don't needed it, and Add the notaion and inject ProductREquest class on index method - ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,36 +8,13 @@ class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @param App\Http\Requests\ProductRequest $request
      */
-    public function index() {}
+    public function index(ProductRequest $request) {}
 
     /**
      * Show the form for creating a new resource.
      */
     public function create() {}
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(ProductRequest $request) {}
-
-    /**
-     * Display the specified resource.
-     */
-    public function show() {}
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit() {}
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(ProductRequest $request) {}
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy() {}
 }
