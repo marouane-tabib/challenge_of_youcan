9cd588b7c9af77cccafa24309b049715e90a53d3
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-12 04:54:31 +0100

    YC-1 | Add the Injection of  ProductServiceInterface and use it on index method to return all products - ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,15 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProductRequest;
+use App\Interfaces\ServiceInterfaces\ProductServiceInterface;
 
 class ProductController extends Controller
 {
+    protected ProductServiceInterface $productService;
+
+    public function __construct(ProductServiceInterface $productService)
+    {
+        $this->productService = $productService;
+    }
+
     /**
      * Display a listing of the resource.
      *
      * @param App\Http\Requests\ProductRequest $request
      */
-    public function index(ProductRequest $request) {}
+    public function index(ProductRequest $request)
+    {
+        return view(
+            'products.index',
+            [
+                'products' => $this->productService->index($request->validated()),
+                'categories' => [],
+            ]
+        );
+    }
 
     /**
      * Show the form for creating a new resource.
