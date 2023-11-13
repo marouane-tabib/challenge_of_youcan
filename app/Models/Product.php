6db0a039d48c348d0bd3038904b4a6d9398594a7
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-13 19:36:02 +0100

    MakeCategoryClasses | Add the relationship bettwen Category and Product classes - Models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
@@ -14,6 +15,11 @@ class Product extends Model
         'name',
         'description',
         'price',
-        'category_id'
+        'category_id',
     ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
