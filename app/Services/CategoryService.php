06b607bbd91481cd9c52432f43fa1ef7086ea056
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-13 19:36:40 +0100

    MakeCategoryClasses | Fixe the loading Error - CategoryService

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -3,13 +3,14 @@
 namespace App\Services;
 
 use App\Abstracts\AbstractBaseResourceService;
+use App\Interfaces\RepositoryInterfaces\CategoryRepositoryInterface;
 use App\Interfaces\ServiceInterfaces\CategoryServiceInterface;
 
 class CategoryService extends AbstractBaseResourceService implements CategoryServiceInterface
 {
     protected $repository;
 
-    public function __construct(CategoryServiceInterface $repository)
+    public function __construct(CategoryRepositoryInterface $repository)
     {
         $this->repository = $repository;
     }
