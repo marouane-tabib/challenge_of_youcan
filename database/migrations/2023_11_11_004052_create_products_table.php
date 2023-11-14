71d26d4a01017dea20a7430572f3cb923790f8dc
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-14 12:56:33 +0100

    AddSubCategory | Add the subCategory Model & database files

--- database/migrations/2023_11_11_004052_create_products_table.php
+++ database/migrations/2023_11_11_004052_create_products_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -18,7 +17,7 @@ return new class extends Migration
             $table->text('description', 5000);
             $table->decimal('price', 8, 2);
             $table->unsignedBigInteger('category_id');
-            // $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
