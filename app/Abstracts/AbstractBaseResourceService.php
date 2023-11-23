c951f47d1b91d30a3c4438f30409408aa57255c2
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-23 18:27:45 +0100

    RefactoreAbstractsPonts | Add ImageUploaderTrait and use it on create method with condition - AbstractBaseResourceService

--- app/Abstracts/AbstractBaseResourceService.php
+++ app/Abstracts/AbstractBaseResourceService.php
@@ -4,10 +4,14 @@ namespace App\Abstracts;
 
 use App\Interfaces\BaseResourceRepositoryInterface;
 use App\Interfaces\BaseResourceServiceInterface;
+use App\Traits\ImageUploaderTrait;
 
 abstract class AbstractBaseResourceService implements BaseResourceServiceInterface
 {
+    use ImageUploaderTrait;
     protected $repository;
+    protected string $storagepath = 'storage/media';
+    protected string $fileName = 'file';
 
     public function __construct(BaseResourceRepositoryInterface $repository)
     {
@@ -21,6 +25,10 @@ abstract class AbstractBaseResourceService implements BaseResourceServiceInterfa
 
     public function create(array $data = [])
     {
+        if (isset($data[$this->fileName])) {
+            $data[$this->fileName] = $this->uploadFile($this->storagepath, $data[$this->fileName]);
+        }
+
         return $this->repository->create($data);
     }
 }
