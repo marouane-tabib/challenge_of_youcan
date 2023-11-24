213bda3e442d81547f9e0c1475d280e7e9c1066a
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-24 14:18:31 +0100

    RefactoreProject | Add the filterValdiation method and storeValidation within validation rules - Services/ValidationServices/ProductValidationService

--- app/Services/ValidationServices/ProductValidationService.php
+++ app/Services/ValidationServices/ProductValidationService.php
@@ -7,4 +7,37 @@ use Illuminate\Support\Facades\Validator;
 
 class ProductValidationService implements ProductValidationServiceInterface
 {
+    public function filterValidation(array $data)
+    {
+        $validator = Validator::make($data, [
+            'sort_by' => 'nullable|string',
+            'order_by' => 'nullable|string',
+            'category_filter' => 'nullable|numeric',
+        ]);
+
+        if ($validator->fails()) {
+            // throw new \Exception($validator->errors()->first($attribute));
+            throw new \InvalidArgumentException($validator->errors()->first());
+        }
+
+        return $data;
+    }
+
+    public function storeValidation(array $data)
+    {
+        $validator = Validator::make($data, [
+            'image' => 'nullable|image|max:7024',
+            'name' => 'required|string|min:3|max:55',
+            'description' => 'required|string|min:10',
+            'category_id' => 'required|exists:categories,id',
+            'price' => 'required|numeric|min:1',
+        ]);
+
+        if ($validator->fails()) {
+            // throw new \Exception($validator->errors()->first($attribute));
+            throw new \InvalidArgumentException($validator->errors()->first());
+        }
+
+        return $data;
+    }
 }
