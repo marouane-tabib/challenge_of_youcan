10f384cfb50feb3e83795a810a9bd42c5360214b
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-24 14:16:40 +0100

    RefactoreProject | Add the bind the ProductValidationServiceInterface to the ProductValidationService - AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,10 @@ namespace App\Providers;
 
 use App\Interfaces\ServiceInterfaces\CategoryServiceInterface;
 use App\Interfaces\ServiceInterfaces\ProductServiceInterface;
+use App\Interfaces\ValidationServiceInterfaces\ProductValidationServiceInterface;
 use App\Services\CategoryService;
 use App\Services\ProductService;
+use App\Services\ValidationServices\ProductValidationService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        $this->app->bind(ProductValidationServiceInterface::class, ProductValidationService::class);
         $this->app->bind(ProductServiceInterface::class, ProductService::class);
         $this->app->bind(CategoryServiceInterface::class, CategoryService::class);
     }
