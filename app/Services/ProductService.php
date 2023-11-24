b860ed743979a4f3eea8e5c3a6dc41d591c2ad7a
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-24 14:31:40 +0100

    RefactoreProject | Add the Injecte ProductValidationServiceInterface and utilized it in the filter method - Services/ProductService.

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -5,20 +5,27 @@ namespace App\Services;
 use App\Abstracts\AbstractBaseResourceService;
 use App\Interfaces\RepositoryInterfaces\ProductRepositoryInterface;
 use App\Interfaces\ServiceInterfaces\ProductServiceInterface;
+use App\Interfaces\ValidationServiceInterfaces\ProductValidationServiceInterface;
 
 class ProductService extends AbstractBaseResourceService implements ProductServiceInterface
 {
+    protected $validator;
     protected $repository;
     protected string $storagepath = 'storage/media/images/products';
     protected string $fileName = 'image';
 
-    public function __construct(ProductRepositoryInterface $repository)
-    {
+    public function __construct(
+        ProductRepositoryInterface $repository,
+        ProductValidationServiceInterface $validator
+    ) {
         $this->repository = $repository;
+        $this->validator = $validator;
     }
 
     public function filter(array $data)
     {
+        $data = $this->validator->filterValidation($data);
+
         return $this->repository->filter($data);
     }
 }
