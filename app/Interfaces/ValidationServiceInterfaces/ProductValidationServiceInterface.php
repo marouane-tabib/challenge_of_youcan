72fb67e439ca4fcab71747e4e0760126cdde3cdc
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-24 14:14:37 +0100

    RefactoreProject | Make app\Interfaces\ValidationServiceInterfaces\ProductValidationServiceInterface.php file, and extends BaseValidationServiceInterface interface

---
+++ app/Interfaces/ValidationServiceInterfaces/ProductValidationServiceInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Interfaces\ValidationServiceInterfaces;
+
+use App\Interfaces\BaseValidationServiceInterface;
+
+interface ProductValidationServiceInterface extends BaseValidationServiceInterface {}
