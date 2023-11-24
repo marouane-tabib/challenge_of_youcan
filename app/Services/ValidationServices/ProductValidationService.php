e05f4b8057fb6d7bc50dbb095e12348b18263f71
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-24 14:17:43 +0100

    RefactoreProject | Make app\Services\ValidationServices\ProductValidationService.php file

---
+++ app/Services/ValidationServices/ProductValidationService.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Services\ValidationServices;
+
+use App\Interfaces\ValidationServiceInterfaces\ProductValidationServiceInterface;
+use Illuminate\Support\Facades\Validator;
+
+class ProductValidationService implements ProductValidationServiceInterface
+{
+}
