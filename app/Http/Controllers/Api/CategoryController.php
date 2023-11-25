02f705af4b7728274cdd82789e3c976486ed2a05
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-25 21:58:52 -0200

    AddVueUI | Add the CategoryServiceInterface injection - CategoryController

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Interfaces\ServiceInterfaces\CategoryServiceInterface;
 
 class CategoryController extends Controller
 {
-    // 
+    protected CategoryServiceInterface $categoryService;
+
+    public function __construct(CategoryServiceInterface $categoryService)
+    {
+        $this->categoryService = $categoryService;
+    }
 }
