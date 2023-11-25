69d9da6d876cf6a5a46df0fd3e8f9d9a72f97c65
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-25 14:09:01 +0100

    RefactoreProject | Add error condition to display an alert - products/index.blade

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -3,6 +3,15 @@
 
 @section('content')
     @include('components.filter')
+
+    @if ($errors->all())
+        @foreach ($errors->all() as $error)
+            <div class="alert alert-danger p-1 px-3 my-2" role="alert">
+                {{ $error }}
+            </div>
+        @endforeach
+    @endif
+
     @include('components.modal')
     @include('components.table')
 @endsection
