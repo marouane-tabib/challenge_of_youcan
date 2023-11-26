3dae3632b0d5de780da22883e47722d15aade481
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-25 22:06:03 -0200

    AddVueUI | Add vite components - resources/layouts/app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     {{-- @vite(['resources/sass/main.scss', 'resources/js/app.js']) --}}
 
     <title>@yield('title' , 'Unknown')</title>
