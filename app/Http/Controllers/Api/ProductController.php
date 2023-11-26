7705acf1760cef1238c8c21b3ee1f22ae83f71fc
marwan-tabib <marwanmaroc297@gmail.com>
2023-11-25 22:04:31 -0200

    AddVueUI | Add the store method to create a new product and return either a success message or handle exceptions - ProductController

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Interfaces\ServiceInterfaces\ProductServiceInterface;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
@@ -12,5 +13,18 @@ class ProductController extends Controller
     public function __construct(ProductServiceInterface $productService)
     {
         $this->productService = $productService;
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $this->productService->create($request->all());
+        } catch (\InvalidArgumentException $e) {
+            return response()->json(['error' => $e->getMessage()]);
+        } catch (\Exception $e) {
+            return response()->json(['exception' => $e->getMessage()]);
+        }
+
+        return response()->json(["message" => "Product Created!"], 200);
     }
 }
